9a9eba422c2a1e67555e7e2e2064c2bdba2c598f
Lacaz <106093900+LACAZ1601@users.noreply.github.com>
2022-08-23 11:32:29 -0300

    +-110

--- CSharp_Aula1/Form1.Designer.cs
+++ CSharp_Aula1/Form1.Designer.cs
@@ -28,13 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.txtNomecompleto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(224, 84);
+            this.txtNome.Multiline = true;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(217, 41);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Location = new System.Drawing.Point(224, 157);
+            this.txtSobrenome.Multiline = true;
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(217, 41);
+            this.txtSobrenome.TabIndex = 0;
+            // 
+            // txtNomecompleto
+            // 
+            this.txtNomecompleto.Location = new System.Drawing.Point(19, 391);
+            this.txtNomecompleto.Multiline = true;
+            this.txtNomecompleto.Name = "txtNomecompleto";
+            this.txtNomecompleto.Size = new System.Drawing.Size(426, 49);
+            this.txtNomecompleto.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sobrenome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 335);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(278, 39);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Nome Completo:";
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSoma.Location = new System.Drawing.Point(224, 254);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(221, 55);
+            this.btnSoma.TabIndex = 2;
+            this.btnSoma.Text = "Juntar ";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(610, 82);
+            this.txtN1.Multiline = true;
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(217, 41);
+            this.txtN1.TabIndex = 0;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(610, 155);
+            this.txtN2.Multiline = true;
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(217, 41);
+            this.txtN2.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(503, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 41);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "N1:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(503, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 41);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "N2:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(869, 255);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(231, 53);
+            this.txtResultado.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(611, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 53);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Soma";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1150, 523);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.txtNomecompleto);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
             this.Text = "Lacaz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.TextBox txtNomecompleto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CSharp_Aula1/Form1.cs
+++ CSharp_Aula1/Form1.cs
@@ -16,5 +16,43 @@ namespace CSharp_Aula1
         {
             InitializeComponent();
         }
+        string nome;
+        string sobrenome;
+        float N1, N2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nome = txtNome.Text;
+            sobrenome = txtSobrenome.Text;
+            nome = nome + " " + sobrenome;
+            txtNomecompleto.Text = nome;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            N1 = float.Parse(txtN1.Text);
+            N2 = float.Parse(txtN2.Text);
+            N1 = N1 + N2;
+            txtResultado.Text = N1.ToString();
+            N1 = float.Parse(txtN1.Text);
+            if (N1 >= 10)
+            {
+                txtResultado.Text = "É um número maior que dez";
+            }
+            else
+            {
+                txtResultado.Text = "É um número menor que dez";
+            }
+        }
     }
 }
